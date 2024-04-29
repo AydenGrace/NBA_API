c0440a69f28e2681db789edd0eb6be9c56525ad3
AydenGrace <pa.ce.hepl@gmail.com>
2024-04-29 15:12:42 +0200

    Front Link

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const config = require("./database/config");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+const allowedOrigin = "https://nba-frontend-sand.vercel.app";
 
 app.use(express.json());
-app.use(
-  cors({
-    origon: "*",
-  })
-);
+// app.use(
+//   cors({
+//     origon: "*",
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigin);
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 const routes = require("./routes");
 app.use(routes);
@@ -22,7 +31,7 @@ mongoose
   })
   .catch((e) => console.error(e));
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server started on port ${PORT}`);
   console.log("\x1b[36m%s\x1b[0m", `http://localhost:${PORT}/api/teams`);
   console.log("\x1b[36m%s\x1b[0m", `http://localhost:${PORT}/api/players`);
